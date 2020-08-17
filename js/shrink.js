374dd2c7352439540f4ee66b0ad2eeef6eeade37
ALNPRA123 <50035795+ALNPRA123@users.noreply.github.com>
2020-08-17 14:30:45 +0700

    dfhd

--- js/shrink.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "35px 10px";
-  } else {
-    document.getElementById("navbar").style.padding = "30px 10px";
-  }
-}
