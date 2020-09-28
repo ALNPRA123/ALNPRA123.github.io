9b915ffaf10d65e62ee006ed46ab3f9d2516918b
ALNPRA123 <50035795+ALNPRA123@users.noreply.github.com>
2020-09-29 01:52:15 +0700

    Release Version 1.3

--- js/dist/shrink.dev.js
+++ js/dist/shrink.dev.js
@@ -1,4 +1,8 @@
-window.onscroll = function() {scrollFunction()}
+"use strict";
+
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
