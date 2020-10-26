3005053d9abe0315c8d73632d8fe6d008c45aba5
ALNPRA123 <50035795+ALNPRA123@users.noreply.github.com>
2020-10-26 14:38:14 +0700

    Update New Fitur

---
+++ js/shrink.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "10px 10px";
+  } else {
+    document.getElementById("navbar").style.padding = "5px 10px";
+  }
+}
