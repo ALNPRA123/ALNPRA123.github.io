ad659413dd4a16f11011decc2a9305ab19fdc274
ALNPRA123 <50035795+ALNPRA123@users.noreply.github.com>
2021-01-07 11:17:31 +0700

    Update Database V(2021)

---
+++ js/dist/shrink.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "10px 10px";
+  } else {
+    document.getElementById("navbar").style.padding = "5px 10px";
+  }
+}
---
+++ js/login.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDgbrz97xl_8_gKrN-Fya2PHW7oBwODPIA",
+    authDomain: "user-2021.firebaseapp.com",
+    databaseURL: "https://user-2021-default-rtdb.firebaseio.com",
+    projectId: "user-2021",
+    storageBucket: "user-2021.appspot.com",
+    messagingSenderId: "1086291048443",
+    appId: "1:1086291048443:web:1433cad1c203b67ee53d5e",
+    measurementId: "G-1Y3KB15J11"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var firebaseRef = firebase.database().ref('emails');
+  document.querySelector('#submitBtn').addEventListener('click', ()=>{
+    const email = document.getElementById('email').value;
+    firebaseRef.push(email);
+  });
