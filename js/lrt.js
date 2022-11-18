f78bae5fa5d8614563a5721e377e26669d9fe6af
ALNPRA123 <aldinurpratama89@gmail.com>
2022-11-18 17:01:19 +0700

    Update Beta-Testing 18/11/22
    Mastering database from firebase

--- js/lrt.js
+++ js/lrt.js
@@ -1,3 +1,12 @@
-function lrt() {
+function InsertData() {
+  alert("Thank You For Subscribe");
+}
+function SelectData() {
+  alert("Thank You For Subscribe");
+}
+function UpdateData() {
+  alert("Thank You For Subscribe");
+}
+function DeleteData() {
   alert("Thank You For Subscribe");
 }
