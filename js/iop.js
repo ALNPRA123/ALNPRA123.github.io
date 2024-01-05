2017b59863f0d8f57fd532fcdcffc998fc9f20f5
ALNPRA123 <50035795+ALNPRA123@users.noreply.github.com>
2024-01-05 16:49:55 +0700

    Update iop.js

--- js/iop.js
+++ js/iop.js
@@ -8,7 +8,7 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function() {z
   popupcontact.style.display = "block";
 };
 
